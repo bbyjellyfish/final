49f460d6cba02a9528baa1de4391cb4aa49ddd1c
bbyjellyfish <139634651+bbyjellyfish@users.noreply.github.com>
2023-12-15 14:30:30 +0300

    Update SQL.py
    доработка

--- SQL.py
+++ SQL.py
@@ -1,16 +1,25 @@
 # Работа с базой данных
 
 # Задание 1
+# Текст задания: "Для этого: выведи список логинов курьеров с количеством их заказов
+# в статусе «В доставке» (поле inDelivery = true)."
+#Поэтому оставляю в запросе вывод этого поля, ошибку с названием другого поля поправила
 
-SELECT c.id, COUNT(."inDelivery")
+SELECT c.login, COUNT(o."inDelivery")
 FROM "Couriers" AS c
-JOIN "Orders" AS o ON c.id = o."couriersId"
+  JOIN "Orders" AS o ON c.id = o."couriersId"
 WHERE o."inDelivery" = True
-GROUP BY c.id;
+GROUP BY c.login;
 
 # Задание 2
+# Синтаксис поправила, в задании указано: "Для этого: выведи все трекеры заказов и их статусы."
+# Поэтому оставляю вывод поля со статусом заказа
 
-SELECT track, finished CASE WHEN finished = True THEN '2' ELSE '0'
-cancelled CASE WHEN cancelled = True THEN '-1' ELSE '0'
-'inDelivery' CASE WHEN 'inDelivery' = True THEN '1' ELSE '0'
+SELECT track, finished, cancelled, 'inDelivery'
+CASE
+WHEN finished = True THEN '2',
+WHEN cancelled = True THEN '-1',
+WHEN 'inDelivery' = True THEN '1'
+ELSE '0'
+END
 FROM 'Orders';
